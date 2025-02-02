74386badae0a2b4f64102d3f959fabc1604224a7
ZorAnderius <gabriel.zorander@gmail.com>
2025-02-02 14:02:37 +0000

    add readme

--- task1.py
+++ task1.py
@@ -31,16 +31,24 @@ def measure_time(sort_function, arr):
 
 if __name__ == "__main__":
     sizes = [10_000, 50_000, 100_000, 500_000]
-    results = {'randomized': [], 'deterministic': []}
+    results = {'randomized': [],'randomized_sorted': [], 'deterministic': [], 'deterministic_sorted': []}
 
     for size in sizes:
         arr = [random.randint(1, 1000000) for _ in range(size)]
 
+        sort_arr = sorted(arr)
         # Час для рандомізованого QuickSort
         rand_time = measure_time(randomized_quick_sort, arr)
 
         # Час для детермінованого QuickSort
         det_time = measure_time(deterministic_quick_sort, arr)
+
+        rand_sorted_time = measure_time(randomized_quick_sort, sort_arr)
+
+        det_sorted_time = measure_time(deterministic_quick_sort, sort_arr)
+
+        results['randomized_sorted'].append(rand_sorted_time)
+        results['deterministic_sorted'].append(det_sorted_time)
 
         results['randomized'].append(rand_time)
         results['deterministic'].append(det_time)
@@ -49,9 +57,14 @@ if __name__ == "__main__":
         print(f"Розмір масиву: {size}")
         print(f"   Рандомізований QuickSort: {results['randomized'][i]:.4f} секунд")
         print(f"   Детермінований QuickSort: {results['deterministic'][i]:.4f} секунд")
+        print(f"   Рандомізований QuickSort (сортування вже відсортованого масиву): {results['randomized_sorted'][i]:.4f} секунд")
+        print(f"   Детермінований QuickSort (сортування вже відсортованого масиву): {results['deterministic_sorted'][i]:.4f} секунд")
 
     plt.plot(sizes, results['randomized'], label='Рандомізований QuickSort', marker='o')
     plt.plot(sizes, results['deterministic'], label='Детермінований QuickSort', marker='x')
+
+    plt.plot(sizes, results['randomized_sorted'], label='Рандомізований QuickSort (сортування вже відсортованого масиву)', marker='o')
+    plt.plot(sizes, results['deterministic_sorted'], label='Детермінований QuickSort (сортування вже відсортованого масиву)', marker='x')
 
     plt.xlabel('Розмір масиву')
     plt.ylabel('Час виконання (секунди)')
